ddd6658313c9ad4fde000b188d447d6a91e71ad0
Ruth Blackman <rkjb410@gmail.com>
2021-11-21 00:58:20 +0000

    Added basic contact form

--- Gemfile
+++ Gemfile
@@ -61,5 +61,7 @@ gem 'modernizr-rails'
 gem 'rails-controller-testing'
 gem 'jquery-rails'
 
+gem 'rails-controller-testing'
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#home'
   get 'response' , to: 'home#responsive'
+  get 'contact' , to: 'home#contact'
+  post 'request_contact' , to: 'home#request_contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
